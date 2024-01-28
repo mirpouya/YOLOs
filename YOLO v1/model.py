bb147c925cb2ebc528d1c2c652169df3cb09e38c
Pouya Mirmohammadsadeghi <pouya.mmsadeghi97@gmail.com>
2024-01-27 20:40:25 -0800

    model update

--- YOLO v1/model.py
+++ YOLO v1/model.py
@@ -2,4 +2,30 @@ import torch
 import torch.nn as nn
 
 # model architecture
-print("new change")
+"""
+architecture config:
+(kernel_size, filters, stride, padding)
+'M' is maxpooling with stride 2*2 and kernel 2*2
+"""
+
+architecture_config = [
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
