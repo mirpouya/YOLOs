b00b013f6caf092e833d348c89a034f19f7f4a10
Pouya Mirmohammadsadeghi <pouya.mmsadeghi97@gmail.com>
2024-01-27 21:45:20 -0800

    CNNBlock add

--- YOLO v1/model.py
+++ YOLO v1/model.py
@@ -29,3 +29,13 @@ architecture_config = [
     (3, 1024, 1, 1),
 ]
 
+# building convolution neural network: Conv, BatchNorm, Relu
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
+        self.batchnorm_2d = nn.BatchNorm2d(out_channels)
+        self.leakyrelu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        return self.leakyrelu(self.batchnorm_2d(self.conv(x)))
